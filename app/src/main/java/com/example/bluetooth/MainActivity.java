8ced862412fd54e9ddd7d99ca2b63987cab09626
Mesut nadir Seyfelioglu <mesutnadirseyfelioglu@Mesuts-MacBook-Pro.local>
2021-02-18 16:56:12 +0300

    inital

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -5,170 +5,172 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.net.IpSecManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int REQUEST_ACCESS_COARSE_LOCATION = 1;
-    public static final int REQUEST_ENABLE_BLUETOOTH = 11;
-    private ListView devicesList;
-    private Button Scan;
-    private BluetoothAdapter bluetoothAdapter;
-    private ArrayAdapter<String> listAdapter;
-
-    Handler handler = new Handler();
-    Runnable refresh;
-
-
+    BluetoothManager btManager;
+    BluetoothAdapter btAdapter;
+    BluetoothLeScanner btScanner;
+    Button startScanningButton;
+    Button stopScanningButton;
+    TextView peripheralTextView;
+    private final static int REQUEST_ENABLE_BT = 1;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    public List<ScanFilter> filters;
+    ScanFilter filter;
+    ScanSettings scanSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        devicesList = findViewById(R.id.devicesList);
-        Scan = findViewById(R.id.Scan);
-
-        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        devicesList.setAdapter(listAdapter);
-
-        refresh = new Runnable() {
-            @Override
-            public void run() {
-                if (bluetoothAdapter != null && bluetoothAdapter.isEnabled()){
-                    if(checkCoarseLocationPermission()){
-                        listAdapter.clear();
-                        bluetoothAdapter.startDiscovery();
-                    }
-                }else {
-                    checkBluethoothState();
-                }
-                handler.postDelayed(refresh,500);
-            }
-        };
-        handler.post(refresh);
-
-        checkBluethoothState();
+        filters = new ArrayList<ScanFilter>();
+        ScanFilter AP1 = new ScanFilter.Builder().setDeviceAddress("BC:F3:10:BA:88:0F").build();
+        ScanFilter AP2 = new ScanFilter.Builder().setDeviceAddress("BC:F3:10:BA:84:4F").build();
+        ScanFilter AP3 = new ScanFilter.Builder().setDeviceAddress("BC:F3:10:20:79:8F").build();
+        //ScanFilter crap = new ScanFilter.Builder().setDeviceAddress("00:00:00:00:00:00").build();
+        filters.add(AP1);
+        filters.add(AP2);
+        filters.add(AP3);
+        //filters.add(crap);
+        scanSettings = new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build();
 
 
 
-        /*Scan.setOnClickListener(new View.OnClickListener() {
-            @Override
+        peripheralTextView = (TextView) findViewById(R.id.PeripheralTextView);
+        peripheralTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        startScanningButton = (Button) findViewById(R.id.StartScanButton);
+        startScanningButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (bluetoothAdapter != null && bluetoothAdapter.isEnabled()){
-                    if(checkCoarseLocationPermission()){
-                        listAdapter.clear();
-                        bluetoothAdapter.startDiscovery();
-                    }
-                }else {
-                    checkBluethoothState();
-                }
+                startScanning();
             }
-        });*/
+        });
 
-        checkCoarseLocationPermission();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(devicesFoundReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
-        registerReceiver(devicesFoundReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED));
-        registerReceiver(devicesFoundReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
-    }
-
-    protected void onPause(){
-        super.onPause();
-        unregisterReceiver(devicesFoundReceiver);
-
-    }
-
-    private boolean checkCoarseLocationPermission(){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_ACCESS_COARSE_LOCATION);
-            return false;
-        }else {
-            return true;
-        }
-    }
-    private void checkBluethoothState(){
-        if (bluetoothAdapter == null){
-            Toast.makeText(this, "Bluetooth is not supported on your divece!", Toast.LENGTH_SHORT).show();
-        }else {
-            if(bluetoothAdapter.isEnabled()){
-                if(bluetoothAdapter.isDiscovering()){
-                    Toast.makeText(this,"Device discovering process ...",Toast.LENGTH_SHORT).show();
-
-                }else {
-                    Toast.makeText(this, "Bluetooth is enabled", Toast.LENGTH_SHORT).show();
-                    Scan.setEnabled(true);
-                }
-            }else {
-                Toast.makeText(this, "You need to enable Bluetooth", Toast.LENGTH_SHORT).show();
-                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH);
+        stopScanningButton = (Button) findViewById(R.id.StopScanButton);
+        stopScanningButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                stopScanning();
             }
+        });
+        stopScanningButton.setVisibility(View.INVISIBLE);
+
+        btManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        btAdapter = btManager.getAdapter();
+        btScanner = btAdapter.getBluetoothLeScanner();
+
+
+        if (btAdapter != null && !btAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent,REQUEST_ENABLE_BT);
         }
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_ENABLE_BLUETOOTH){
-            checkBluethoothState();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode){
-            case REQUEST_ACCESS_COARSE_LOCATION :
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Access coarse location allowed. You can scan Bluetooth devices", Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(this, "Access coarse location forbiden. You can't scan Bluetooth devices", Toast.LENGTH_SHORT).show();
+        // Make sure we have access coarse location enabled, if not, prompt the user to enable it
+        if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("This app needs location access");
+            builder.setMessage("Please grant location access so this app can detect peripherals.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
                 }
-                break;
+            });
+            builder.show();
         }
     }
 
-    private final BroadcastReceiver devicesFoundReceiver = new BroadcastReceiver() {
-
+    // Device scan callback.
+    private ScanCallback leScanCallback = new ScanCallback() {
         @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                //rssi part
-                short rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
-                listAdapter.add(device.getName() +"\n" + device.getAddress()+"\n"+ rssi +" dBm");
-                listAdapter.notifyDataSetChanged();
-            }else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                Scan.setText("Scanning Bluetooth Devices");
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                Scan.setText("Scanning in progress");
-            }
+        public void onScanResult(int callbackType, ScanResult result) {
+
+            peripheralTextView.append("Device Name: " + result.getDevice() + " rssi: " + result.getRssi() + "\n");
+
+            // auto scroll for text view
+            final int scrollAmount = peripheralTextView.getLayout().getLineTop(peripheralTextView.getLineCount()) - peripheralTextView.getHeight();
+            // if there is no need to scroll, scrollAmount will be <=0
+            if (scrollAmount > 0)
+                peripheralTextView.scrollTo(0, scrollAmount);
         }
     };
-}
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    System.out.println("coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
+    }
+
+    public void startScanning() {
+        System.out.println("start scanning");
+        peripheralTextView.setText("");
+        startScanningButton.setVisibility(View.INVISIBLE);
+        stopScanningButton.setVisibility(View.VISIBLE);
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                btScanner.startScan(filters, scanSettings, leScanCallback);
+                //btScanner.startScan(leScanCallback);
+            }
+        });
+    }
+
+    public void stopScanning() {
+        System.out.println("stopping scanning");
+        peripheralTextView.append("Stopped Scanning");
+        startScanningButton.setVisibility(View.VISIBLE);
+        stopScanningButton.setVisibility(View.INVISIBLE);
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                btScanner.stopScan(leScanCallback);
+            }
+        });
+    }
+}
